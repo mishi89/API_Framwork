7e3c1765f50b602bc527e1009de3e423bb9fbc2e
ss <ss@DESKTOP-5TCETT7>
2021-01-01 13:06:50 +0530

    API_Framework

---
+++ API_Framwork_Design/src/main/java/API_Framwork_Design/API_Framwork_Design/App.java
@@ -0,0 +1,13 @@
+package API_Framwork_Design.API_Framwork_Design;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
